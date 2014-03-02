da88e4cd80b53bb97686c45f7e4d734a07a267c9
Donald Chan <you@example.com>
2014-03-02 17:56:45 +1100

    Added ability to download Photos from Parse

--- MatchedUp 2/MUSecondViewController.m
+++ MatchedUp 2/MUSecondViewController.m
@@ -9,6 +9,7 @@
 #import "MUSecondViewController.h"
 
 @interface MUSecondViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
 
 @end
 
@@ -18,6 +19,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    PFQuery *query = [PFQuery queryWithClassName:kCCPhotoClassKey];
+    [query whereKey:kCCPhotoUserKey equalTo:[PFUser currentUser]];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if ([objects count] > 0) {
+            PFObject *photo = objects[0];
+            PFFile *pictureFile = photo[kCCPhotoPictureKey];
+            [pictureFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                self.profilePictureImageView.image = [UIImage imageWithData:data];
+            }];
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
